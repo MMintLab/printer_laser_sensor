036a4f65aab1855a668902551d467993446f721e
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 11:19:14 -0500

    commit

--- src/capture_sequence_example.py
+++ src/capture_sequence_example.py
@@ -4,6 +4,18 @@ import time
 
 camera = PiCamera(resolution=(640, 480), framerate=80)
 # Wait for the automatic gain control to settle
+camera.shutter_speed = 10000
+
+#display camera fields
+print(camera.resolution)
+print(camera.framerate)
+print(camera.exposure_speed)
+print(camera.iso)
+print(camera.awb_gains)
+print(camera.awb_mode)
+print(camera.shutter_speed)
+print(camera.exposure_mode)
+
 sleep(2)
 # Now fix the values
 # Finally, take several photos with the fixed settings
