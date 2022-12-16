085b6dd7766ba705343fd8152ed8fb083aa37f27
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 15:26:04 -0500

    commit

--- src/capture_sequence_example_v3.py
+++ src/capture_sequence_example_v3.py
@@ -9,6 +9,24 @@ import numpy as np
 import RPi.GPIO as GPIO
 from smbus import SMBus
 
+#clear files to write to
+f0 = open('camera00time.txt','w')
+f1 = open('camera01time.txt','w')
+f0.close()
+f1.close()
+
+#open file to write to
+f0 = open('camera00time.txt','a')
+f1 = open('camera01time.txt','a')
+
+#store time values in file
+def store_time(camera_number,imagetime):
+    if camera_number==0:
+        f0.write(str(imagetime)+'\n')
+    elif camera_number==1:
+        f1.write(str(imagetime)+'\n')
+
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 GPIO.setup(17, GPIO.OUT)
@@ -41,22 +59,26 @@ print(camera.awb_mode)
 print(camera.shutter_speed)
 print(camera.exposure_mode)
 
-def outputs(numphotos,i2cbus):
+def outputs(numphotos,i2cbus,starttime):
     stream = io.BytesIO()
     for i in range(numphotos):
         yield stream
         switch_camera(i%2,i2cbus)
         #get image from stream in opencv format
+        imagetime = time.time()-starttime
         data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
         img = cv.imdecode(data, 1)
         cv.imwrite('camera%dimage%02d.jpg' % (i%2,int(i/2)), img)
+        store_time(i%2,imagetime)
         stream.seek(0)
         stream.truncate()
 
 # Now fix the values
 # Finally, take several photos with the fixed settings
 starttime = time.time()
-camera.capture_sequence(outputs(100,i2cbus),use_video_port=True,burst=False)
+camera.capture_sequence(outputs(100,i2cbus,starttime),use_video_port=True,burst=False)
 print(time.time()-starttime)
 camera.close()
 GPIO.cleanup()
+f1.close()
+f0.close()
