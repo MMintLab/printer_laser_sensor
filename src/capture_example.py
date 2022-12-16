fa2104904021d0bbc73eb6129c9a34dcda645354
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 10:53:05 -0500

    commit

--- src/capture_example.py
+++ src/capture_example.py
@@ -6,12 +6,5 @@ camera = PiCamera(resolution=(1280, 720), framerate=30)
 camera.iso = 100
 # Wait for the automatic gain control to settle
 sleep(2)
-# Now fix the values
-camera.shutter_speed = camera.exposure_speed
-print(camera.shutter_speed)
-camera.exposure_mode = 'off'
-g = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = g
 # Finally, take several photos with the fixed settings
 camera.capture('image1.jpg')
