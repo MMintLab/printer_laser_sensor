650494c733302bcbab08fa0be94900ac0d24b0f0
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 11:10:52 -0500

    commit

--- src/capture_sequence_example.py
+++ src/capture_sequence_example.py
@@ -1,10 +1,13 @@
 from time import sleep
 from picamera import PiCamera
+import time
 
-camera = PiCamera(resolution=(1280, 720), framerate=30)
+camera = PiCamera(resolution=(640, 480), framerate=80)
 # Wait for the automatic gain control to settle
 sleep(2)
 # Now fix the values
 # Finally, take several photos with the fixed settings
+starttime = time.time()
 camera.capture_sequence(['image%d.jpg' % i for i in range(10)])
+print(time.time()-starttime)
 camera.close()
