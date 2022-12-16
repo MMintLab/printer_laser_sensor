abbd0bafbfc8a1006526f42c246a7617b159b31f
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 11:51:31 -0500

    commit

--- src/capture_sequence_example_v2.py
+++ src/capture_sequence_example_v2.py
@@ -1,6 +1,8 @@
 from time import sleep
 from picamera import PiCamera
 import time
+from PIL import Image
+import PIL.ImageOps as imops
 
 camera = PiCamera(resolution=(640, 480), framerate=80)
 # Wait for the automatic gain control to settle
@@ -17,9 +19,21 @@ print(camera.shutter_speed)
 print(camera.exposure_mode)
 
 sleep(2)
+
+def outputs(numphotos):
+    stream = io.BytesIO()
+    for i in range(numphotos):
+        yield stream
+        img = Image.open(stream)
+        print('inverted')
+        img2 = imops.invert(img)
+        img2.save('image%d.jpg' % i)
+        stream.seek(0)
+        stream.truncate()
+
 # Now fix the values
 # Finally, take several photos with the fixed settings
 starttime = time.time()
-camera.capture_sequence(['image%d.jpg' % i for i in range(10)],use_video_port=True,burst=False)
+camera.capture_sequence(outputs(10),use_video_port=True,burst=False)
 print(time.time()-starttime)
 camera.close()
