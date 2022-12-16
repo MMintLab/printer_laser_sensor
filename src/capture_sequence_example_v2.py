8b6e1623ab009113786f17f47e4dc50a4cd7fd26
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 12:05:28 -0500

    commit

--- src/capture_sequence_example_v2.py
+++ src/capture_sequence_example_v2.py
@@ -5,7 +5,7 @@ from PIL import Image
 import PIL.ImageOps as imops
 import io
 
-camera = PiCamera(resolution=(640, 480), framerate=80)
+camera = PiCamera(resolution=(200, 200), framerate=80)
 # Wait for the automatic gain control to settle
 camera.shutter_speed = 5000
 
