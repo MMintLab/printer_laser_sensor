f87d40cab884e823975f10387189c1962e95f268
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 15:08:29 -0500

    commit

--- src/capture_sequence_example_v3.py
+++ src/capture_sequence_example_v3.py
@@ -49,10 +49,7 @@ def outputs(numphotos,i2cbus):
         #get image from stream in opencv format
         data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
         img = cv.imdecode(data, 1)
-        print('inverted')
-        img2 = img[:, :, 2]
-        #save image
-        cv.imwrite('image%02d.jpg' % i, img2)
+        cv.imwrite('camera%dimage%02d.jpg' % i%2 % int(i/2), img)
         stream.seek(0)
         stream.truncate()
 
