ebfc984df373b0ef7dc03c23bc7b35cba3cb9247
William van den Bogert <william@Williams-MacBook-Pro-34.local>
2022-12-16 10:15:46 -0500

    stuff

---
+++ src/capture_sequence_example.py
@@ -0,0 +1,31 @@
+import io
+import time
+import picamera
+
+from PIL import Image
+
+with picamera.PiCamera() as camera:
+    # Set the camera's resolution to VGA @40fps and give it a couple
+    # of seconds to measure exposure etc.
+    camera.resolution = (640, 720)
+    camera.framerate = 80
+    camera.rotation = 0
+    time.sleep(2)
+    # Set up 40 in-memory streams
+    outputs = [io.BytesIO() for i in range(40)]
+    start = time.time()
+    camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
+
+    finish = time.time()
+    # How fast were we?
+    print('Captured 40 images at %.2ffps' % (40 / (finish - start)))
+
+    count = 0
+    for frameData in outputs:
+        rawIO = frameData
+        rawIO.seek(0)
+        byteImg = Image.open(rawIO)
+
+        count += 1
+        filename = "testimage" + str(count) + ".jpg"
+        byteImg.save(filename, 'JPEG')
