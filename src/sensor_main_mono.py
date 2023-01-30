88539f85598b00cdb9fe83eaa6d1a71a8285c84c
William van den Bogert <willvdb@umich.edu>
2023-01-30 10:24:47 -0500

    added main sensor mono

--- src/sensor_main_mono.py
+++ src/sensor_main_mono.py
@@ -1,13 +1,18 @@
-import cv2
-import sys
-import os
-import numpy as np
-from matplotlib import pyplot as plt
+from time import sleep
+from picamera import PiCamera
 import time
+from PIL import Image
+import PIL.ImageOps as imops
+import io
+import cv2
+import numpy as np
+import RPi.GPIO as GPIO
+from smbus import SMBus
+import rospy
+import sys
 
-data_folder = 'data/Jan11_2023'
-
-#test_number = int(sys.argv[1])
+pub = rospy.Publisher('/bead_area', Float32, queue_size=1)
+rospy.init_node('sensor_node')
 
 def laser_seg(R,G,B,S,V,L):
     weights = np.array([[2.77323259],[-4.28669185],[2.87523104],[-0.40948264],[-2.14548059],[0.69278767],[1.30010453]])
@@ -61,31 +66,6 @@ def get_channels(image):
 
     return red_channel, green_channel, blue_channel, sat_channel, value_channel, lum_channel
 
-#get sorted list of all files in data_foler beginning with test_[test_number] and ending with .jpg
-def get_filename_list(test_number,data_folder):
-    image_list = []
-    #print('test%02d' % (test_number))
-    for filename in os.listdir(data_folder):
-        if filename.startswith(str('test%02d' % (test_number))) and filename.endswith('.jpg'):
-            image_list.append(filename)
-    
-    #sort list of images
-    image_list.sort()
-    return image_list
-
-#sobel edge detection
-def sobel_edge_detection(gray):
-    #sobel edge detection
-    sobelx = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=1)
-    sobely = cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=1)
-    sobel = np.sqrt(sobelx**2 + sobely**2)
-    return sobel
-
-def get_times(test_number,data_folder):
-    filename = 'test%02d_mono_time.txt' % (test_number)
-    #load time data as numpy array from file
-    time_list = np.loadtxt(data_folder+'/'+filename)
-    return time_list
 
 def get_highest_intensity_pixel_indices(image):
     highest_intensity_pixel_indices = np.zeros((1,image.shape[1]))
@@ -100,29 +80,55 @@ def running_average(x, N):
     cumsum = np.cumsum(x, axis=1)
     return (cumsum[:,N:] - cumsum[:,:-N]) / float(N)
 
-test_numbers = [1,2,4,5,6,7,8]
-flow_rates = [2,2,2,4,4,4,4]
-feed_rates = [5,10,10,5,10,5,10]
+capture_framerate = 5
 
-start_times = [4,6,4,4,2,8,6]
-end_times = [8,12,9,9,7,12,12]
+desired_camera_arg = int(sys.argv[1])
 
-beadCSAperFR = [0,0,0,0,0,0,0]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
-STDev = [0,0,0,0,0,0,0]
+i2cbus = SMBus(1)
 
-for j in range(len(test_numbers)):
-    test_number = test_numbers[j]
-    filename_list = get_filename_list(test_number,data_folder)
-    time_list = get_times(test_number,data_folder)
+def switch_camera(desired_camera,i2cbus):
+    if desired_camera==1:
+        i2cbus.write_byte_data(0x70, 0x00, 0x02)
+        GPIO.output(4, GPIO.HIGH)
+        GPIO.output(17, GPIO.LOW)
+    elif desired_camera==0:
+        i2cbus.write_byte_data(0x70, 0x00, 0x01)
+        GPIO.output(4, GPIO.LOW)
+        GPIO.output(17, GPIO.LOW)
 
+camera = PiCamera(resolution=(200, 200), framerate=capture_framerate)
+# Wait for the automatic gain control to settle
 
-    #for all images in filename_list, get red, green, blue, saturation, value, and luminance channels
-    #and create laser and nozzle segmentation images
-    bead_CSA = np.zeros((np.size(time_list)))
-    running_average_number = 10
-    for i in range(len(filename_list)):
-        image = cv2.imread(data_folder+'/'+filename_list[i])
+sleep(2)
+camera.shutter_speed = 50000
+camera.zoom = (0.3,0.3,0.4,0.4)
+camera.iso = 800
+
+#display camera fields
+print(camera.resolution)
+print(camera.framerate)
+print(camera.exposure_speed)
+print(camera.iso)
+print(camera.awb_gains)
+print(camera.awb_mode)
+print(camera.shutter_speed)
+print(camera.exposure_mode)
+
+switch_camera(desired_camera_arg,i2cbus)
+
+def outputs(i2cbus,starttime):
+    stream = io.BytesIO()
+    while not rospy.is_shutdown():
+        yield stream
+        #get image from stream in opencv format
+        imagetime = time.time()-starttime
+        data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+        image = cv2.imdecode(data, 1)
+        #get channels
         red_channel, green_channel, blue_channel, sat_channel, value_channel, lum_channel = get_channels(image)
         laser_seg_image = laser_seg(red_channel,green_channel,blue_channel,sat_channel,value_channel,lum_channel)
         nozzle_seg_image = nozzle_seg(red_channel,green_channel,blue_channel,sat_channel)
@@ -150,42 +156,15 @@ for j in range(len(test_numbers)):
         #running average of highest intensity pixel indices
         highest_intensity_pixel_indices = running_average(highest_intensity_pixel_indices,N=running_average_number)
         highest_intensity_pixel_indices = highest_intensity_pixel_indices - np.min(highest_intensity_pixel_indices)
-        bead_CSA[i] = np.sum(highest_intensity_pixel_indices)
-        #get time of image
-        #plot highest intensity pixel indices vs column number
-        #show laser_seg_image with cv2
-        cv2.imshow('nozzle_seg_image',nozzle_seg_image)
-        cv2.imshow('laser_seg_image',laser_seg_image)
-        #plt.figure(figsize=(2,2))
-        #plt.plot(np.linspace(start=0, stop=image.shape[1]-1-running_average_number, num=image.shape[1]-running_average_number, axis=0),highest_intensity_pixel_indices[0,:])
-        #plt.xlabel('Column Number of Image')
-        #plt.ylabel('Height of Laser Line')
-        #plt.title('Camera Laser Perception %d' % (i))
-        #plt.show()
-        #close plot
-        #plt.close()
-        cv2.waitKey(1)
+        bead_area = np.sum(highest_intensity_pixel_indices)
+        pub.publish(bead_area)
+        stream.seek(0)
+        stream.truncate()
 
-    plt.figure(figsize=(5,5))
-    #plot bead_CSA vs time, ignoring the first and last 20 values
-    plt.plot(time_list[(time_list>start_times[j])&(time_list<end_times[j])],bead_CSA[(time_list>start_times[j])&(time_list<end_times[j])])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Bead Cross Sectional Area (Square Pixels)')
-    plt.title('Bead Cross Sectional Area vs Time'+str(test_number))
-    plt.show()
-    beadCSAperFR[j] = np.mean(bead_CSA[(time_list>start_times[j])&(time_list<end_times[j])]*feed_rates[j])
-    STDev[j] = np.std(bead_CSA[(time_list>start_times[j])&(time_list<end_times[j])]*feed_rates[j])
-    cv2.destroyAllWindows()
-
-plt.figure(figsize=(5,5))
-#plot beadCSAperFR vs flow_rates including error bars
-plt.errorbar(flow_rates,beadCSAperFR,yerr=STDev,fmt='o')
-#plot best fit line
-z = np.polyfit(flow_rates,beadCSAperFR,1)
-p = np.poly1d(z)
-plt.plot(flow_rates,p(flow_rates),"r--")
-#plot best fit line equation
-plt.text(3,600,'y=%.6fx+(%.6f)'%(z[0],z[1]))
-plt.xlabel('Flow Rate (mL/min)')
-plt.ylabel('Bead Cross Sectional Area Times Feed Rate (Square Pixels*mm/s)')
-plt.show()
+# Now fix the values
+# Finally, take several photos with the fixed settings
+starttime = time.time()
+camera.capture_continuous(outputs(i2cbus,starttime),use_video_port=True,burst=False)
+print(time.time()-starttime)
+camera.close()
+GPIO.cleanup()
